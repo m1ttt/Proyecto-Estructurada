18df39ae3f1e58154a0c7696a6bc53189d246675
Miguel Moreno <mmoreno@flexor.mx>
2024-04-30 20:59:47 -0600

    Eliminar función sumar y archivos relacionados

--- src/main.c
+++ src/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-#include "prototipos/funciones.h"
+
 
 int main() {
-    int a = 10;
-    int b = 20;
     
-    printf("La suma de %d y %d es %d\n", a, b, sumar(a, b));
-    
-    return 0;
 }

3c7487def9500b4d9eb12e98ae9231f39e96dfd2
Miguel Moreno <mmoreno@flexor.mx>
2024-05-01 14:13:34 -0600

    .

--- src/main.c
+++ src/main.c
@@ -2,21 +2,10 @@
 #include "prototipos/materiales.h"
 #include "prototipos/menu.h"
 #include <stdio.h>
-/*NOTA:
-Cada pieza define su tipo con una char, siendo:
-P: Peón
-R: Rey
-Q: Reina
-T: Torre
-A: Alfil
-C: Caballo
-// Crear algunas piezas
-    Pieza peonBlanco = {1, 1, 1, 'P', 0, 0}; // CoordenadaX, CoordenadaY, valor, tipo, color, capturada
-    Pieza torreNegra = {1, 6, 5, 'T', 1, 0}; // CoordenadaX, CoordenadaY, valor, tipo, color, capturada
-*/
+
 
 int main() {
   inicializarMenu();
 
-    return 0;
+  return 0;
 }

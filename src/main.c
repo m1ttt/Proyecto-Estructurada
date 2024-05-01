c227c723195d78d20043919f1921b8a5c32fe035
Miguel Moreno <mmoreno@flexor.mx>
2024-04-30 21:00:07 -0600

    Añadir función sumar y mostrar resultado en main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+#include "prototipos/funciones.h"
 
 int main() {
+    int a = 10;
+    int b = 20;
     
+    printf("La suma de %d y %d es %d\n", a, b, sumar(a, b));
+    
+    return 0;
 }

d9d83392b634c8f74fb1ec1e71d8d0d64f866921
Miguel Moreno <mmoreno@flexor.mx>
2024-05-01 14:37:44 -0600

    cambios insanos

--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 #TODO
 - Embeber las librerias de ncurses para win y unx
+- GTK como solucionador de papus a ncurses
--- src/funciones/menu.c
+++ src/funciones/menu.c
@@ -1,22 +1,29 @@
 #include "../prototipos/menu.h"
 #include "../prototipos/sistema.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 
+/*NOTA:
+Cada pieza define su tipo con una char, siendo:
+P: Peón
+R: Rey
+Q: Reina
+T: Torre
+A: Alfil
+C: Caballo
+// Crear algunas piezas
+    Pieza peonBlanco = {1, 1, 1, 'P', 0, 0}; // CoordenadaX, CoordenadaY, valor,
+tipo, color, capturada Pieza torreNegra = {1, 6, 5, 'T', 1, 0}; // CoordenadaX,
+CoordenadaY, valor, tipo, color, capturada
+*/
+
 void jugar() { limpiezaDePantalla(); }
 
-void salir() {
-
-  exit(0);
-}
+void salir() { exit(0); }
 
 OpcionesMenu opciones[] = {{"Jugar", jugar}, {"Salir", salir}};
 
-void inicializarMenu() {
-
-
-}
+void inicializarMenu() {}
 
 void limpiezaDePantalla() {
   int sistema = detectorDeSistema();

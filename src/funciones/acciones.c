830f8fee959a59a99f138ba298c1568f7cb34e5d
Miguel Moreno <mmoreno@flexor.mx>
2024-05-01 12:20:18 -0600

    Identacion y codigo de menu

--- src/funciones/acciones.c
+++ src/funciones/acciones.c
@@ -1,83 +1,89 @@
 #include "../prototipos/materiales.h"
 #include <stdio.h>
 
-
 void agregarMovimiento(int x, int y) {
-    if (x >= 0 && x < 8 && y >= 0 && y < 8) {  // Verifica que el movimiento esté dentro del tablero
-        posiblesMovimientos[numMovimientos].x = x;
-        posiblesMovimientos[numMovimientos].y = y;
-        numMovimientos++;
-    }
+  if (x >= 0 && x < 8 && y >= 0 &&
+      y < 8) { // Verifica que el movimiento esté dentro del tablero
+    posiblesMovimientos[numMovimientos].x = x;
+    posiblesMovimientos[numMovimientos].y = y;
+    numMovimientos++;
+  }
 }
 
 void obtenerMovimientos(Pieza p) {
-    numMovimientos = 0;  // Reinicia la cuenta de movimientos
-
-    switch (p.tipo) {
-        case 'P':  // Peón
-            // Movimientos típicos hacia adelante; no incluye captura ni promoción
-            agregarMovimiento(p.coordenadaX, p.coordenadaY + (p.color == 0 ? 1 : -1));
-            break;
-        case 'R':  // Rey
-            for (int dx = -1; dx <= 1; dx++) {
-                for (int dy = -1; dy <= 1; dy++) {
-                    if (dx != 0 || dy != 0) {
-                        agregarMovimiento(p.coordenadaX + dx, p.coordenadaY + dy);
-                    }
-                }
-            }
-            break;
-        case 'Q':  // Reina
-            for (int i = 1; i < 8; i++) {  // Movimientos horizontales, verticales y diagonales
-                agregarMovimiento(p.coordenadaX + i, p.coordenadaY);
-                agregarMovimiento(p.coordenadaX - i, p.coordenadaY);
-                agregarMovimiento(p.coordenadaX, p.coordenadaY + i);
-                agregarMovimiento(p.coordenadaX, p.coordenadaY - i);
-                agregarMovimiento(p.coordenadaX + i, p.coordenadaY + i);
-                agregarMovimiento(p.coordenadaX - i, p.coordenadaY - i);
-                agregarMovimiento(p.coordenadaX + i, p.coordenadaY - i);
-                agregarMovimiento(p.coordenadaX - i, p.coordenadaY + i);
-            }
-            break;
-        case 'T':  // Torre
-            for (int i = 1; i < 8; i++) {  // Movimientos horizontales y verticales
-                agregarMovimiento(p.coordenadaX + i, p.coordenadaY);
-                agregarMovimiento(p.coordenadaX - i, p.coordenadaY);
-                agregarMovimiento(p.coordenadaX, p.coordenadaY + i);
-                agregarMovimiento(p.coordenadaX, p.coordenadaY - i);
-            }
-            break;
-        case 'A':  // Alfil
-            for (int i = 1; i < 8; i++) {  // Movimientos diagonales
-                agregarMovimiento(p.coordenadaX + i, p.coordenadaY + i);
-                agregarMovimiento(p.coordenadaX - i, p.coordenadaY - i);
-                agregarMovimiento(p.coordenadaX + i, p.coordenadaY - i);
-                agregarMovimiento(p.coordenadaX - i, p.coordenadaY + i);
-            }
-            break;
-        case 'C':  // Caballo
-            // Movimientos en L
-            int knightMoves[8][2] = {{2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
-            for (int i = 0; i < 8; i++) {
-                agregarMovimiento(p.coordenadaX + knightMoves[i][0], p.coordenadaY + knightMoves[i][1]);
-            }
-            break;
+  numMovimientos = 0; // Reinicia la cuenta de movimientos
+  int knightMoves[8][2] = {{2, 1},   {1, 2},   {-1, 2}, {-2, 1},
+                           {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
+  switch (p.tipo) {
+  case 'P': // Peón
+    // Movimientos típicos hacia adelante; no incluye captura ni promoción
+    agregarMovimiento(p.coordenadaX, p.coordenadaY + (p.color == 0 ? 1 : -1));
+    break;
+  case 'R': // Rey
+    for (int dx = -1; dx <= 1; dx++) {
+      for (int dy = -1; dy <= 1; dy++) {
+        if (dx != 0 || dy != 0) {
+          agregarMovimiento(p.coordenadaX + dx, p.coordenadaY + dy);
+        }
+      }
     }
+    break;
+  case 'Q': // Reina
+    for (int i = 1; i < 8;
+         i++) { // Movimientos horizontales, verticales y diagonales
+      agregarMovimiento(p.coordenadaX + i, p.coordenadaY);
+      agregarMovimiento(p.coordenadaX - i, p.coordenadaY);
+      agregarMovimiento(p.coordenadaX, p.coordenadaY + i);
+      agregarMovimiento(p.coordenadaX, p.coordenadaY - i);
+      agregarMovimiento(p.coordenadaX + i, p.coordenadaY + i);
+      agregarMovimiento(p.coordenadaX - i, p.coordenadaY - i);
+      agregarMovimiento(p.coordenadaX + i, p.coordenadaY - i);
+      agregarMovimiento(p.coordenadaX - i, p.coordenadaY + i);
+    }
+    break;
+  case 'T':                       // Torre
+    for (int i = 1; i < 8; i++) { // Movimientos horizontales y verticales
+      agregarMovimiento(p.coordenadaX + i, p.coordenadaY);
+      agregarMovimiento(p.coordenadaX - i, p.coordenadaY);
+      agregarMovimiento(p.coordenadaX, p.coordenadaY + i);
+      agregarMovimiento(p.coordenadaX, p.coordenadaY - i);
+    }
+    break;
+  case 'A':                       // Alfil
+    for (int i = 1; i < 8; i++) { // Movimientos diagonales
+      agregarMovimiento(p.coordenadaX + i, p.coordenadaY + i);
+      agregarMovimiento(p.coordenadaX - i, p.coordenadaY - i);
+      agregarMovimiento(p.coordenadaX + i, p.coordenadaY - i);
+      agregarMovimiento(p.coordenadaX - i, p.coordenadaY + i);
+    }
+    break;
+  case 'C': // Caballo
+    // Movimientos en L
+
+    for (int i = 0; i < 8; i++) {
+      agregarMovimiento(p.coordenadaX + knightMoves[i][0],
+                        p.coordenadaY + knightMoves[i][1]);
+    }
+    break;
+  }
 }
 
-void moverPieza(Tablero* tablero, Pieza* pieza, int newX, int newY) {
-    obtenerMovimientos(*pieza);  // Obtiene los movimientos válidos para la pieza
+void moverPieza(Tablero *tablero, Pieza *pieza, int newX, int newY) {
+  obtenerMovimientos(*pieza); // Obtiene los movimientos válidos para la pieza
 
-    if (esMovimientoValido(newX, newY)) {
-        if (tablero->casillas[newX][newY] == NULL) {  // Verifica que la casilla destino esté vacía
-            tablero->casillas[pieza->coordenadaX][pieza->coordenadaY] = NULL;  // Limpia la casilla actual
-            pieza->coordenadaX = newX;  // Actualiza la posición de la pieza
-            pieza->coordenadaY = newY;
-            tablero->casillas[newX][newY] = pieza;  // Coloca la pieza en la nueva posición
-        } else {
-            printf("La casilla destino (%d, %d) está ocupada.\n", newX, newY);
-        }
+  if (esMovimientoValido(newX, newY)) {
+    if (tablero->casillas[newX][newY] ==
+        NULL) { // Verifica que la casilla destino esté vacía
+      tablero->casillas[pieza->coordenadaX][pieza->coordenadaY] =
+          NULL;                  // Limpia la casilla actual
+      pieza->coordenadaX = newX; // Actualiza la posición de la pieza
+      pieza->coordenadaY = newY;
+      tablero->casillas[newX][newY] =
+          pieza; // Coloca la pieza en la nueva posición
     } else {
-        printf("Movimiento no válido para la pieza %c.\n", pieza->tipo);
+      printf("La casilla destino (%d, %d) está ocupada.\n", newX, newY);
     }
+  } else {
+    printf("Movimiento no válido para la pieza %c.\n", pieza->tipo);
+  }
 }
--- src/funciones/menu.c
+++ src/funciones/menu.c
@@ -1,18 +1,62 @@
 #include "../prototipos/menu.h"
 #include "../prototipos/sistema.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <curses.h>
 #include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void inicializacionDeVariables(){
+typedef struct {
+  char *nombre;
+  void (*funcion)();
+} OpcionesMenu;
 
+void jugar() {}
+
+void salir() {
+  endwin();
+  exit(0);
 }
 
-
+OpcionesMenu opciones[] = {{"Jugar", jugar}, {"Salir", salir}};
 
 void inicializarMenu() {
-  
   limpiezaDePantalla();
+  initscr();
+  noecho();
+  cbreak();
+  keypad(stdscr, TRUE);
+
+  int seleccion = 0;
+  int ch;
+
+  while (1) {
+    for (int i = 0; i < sizeof(opciones) / sizeof(OpcionesMenu); i++) {
+      if (i == seleccion) {
+        attron(A_REVERSE);
+      }
+      mvprintw(i + 1, 1, opciones[i].nombre);
+      attroff(A_REVERSE);
+    }
+
+    ch = getch();
+    switch (ch) {
+    case KEY_UP:
+      seleccion--;
+      if (seleccion < 0) {
+        seleccion = 0;
+      }
+      break;
+    case KEY_DOWN:
+      seleccion++;
+      if (seleccion >= sizeof(opciones) / sizeof(OpcionesMenu)) {
+        seleccion = sizeof(opciones) / sizeof(OpcionesMenu) - 1;
+      }
+      break;
+    case 10:
+      opciones[seleccion].funcion();
+      break;
+    }
+  }
 }
 
 void limpiezaDePantalla() {

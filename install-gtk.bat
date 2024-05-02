2b69c29ffb0cf1ad83241db69522af3d52f5dff4
PapaCerdito1503 <emifragon13@gmail.com>
2024-05-02 08:20:12 -0600

    v4

--- install-gtk.bat
+++ install-gtk.bat
@@ -7,7 +7,7 @@ if %errorlevel% neq 0 (
     echo Instalando MSYS2...
     powershell -command "Invoke-WebRequest -Uri 'https://github.com/msys2/msys2-installer/releases/download/2022-05-24/msys2-x86_64-20220524.exe' -OutFile 'msys2-installer.exe'"
     start /wait msys2-installer.exe
-    set PATH=%PATH%;C:\msys64\usr\bin;C:\msys64\mingw64\bin
+    set "PATH=%PATH%;C:\msys64\usr\bin;C:\msys64\mingw64\bin"
 )
 
 :: Actualizar MSYS2

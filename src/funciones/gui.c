a8759ff5263654e96eb43d99a1f931d596af4021
Miguel Moreno <mmoreno@flexor.mx>
2024-05-01 21:04:58 -0600

    BUG Fixes de GUI y estilos agregados al GUI

--- src/funciones/gui.c
+++ src/funciones/gui.c
@@ -2,9 +2,23 @@
 
 // Función generica para crear una ventana
 GtkWidget *crearVentana(const char *titulo, int ancho, int alto) {
+  GtkCssProvider *provider = gtk_css_provider_new(); // Inicializa el GtkCssProvider
+  GError *error = NULL;
+
   GtkWidget *ventana;
 
   gtk_init(NULL, NULL);
+
+  if (!gtk_css_provider_load_from_path(provider, "src/assets/estilos.css",
+                                       &error)) {
+    g_printerr("Error loading CSS: %s\n", error->message);
+    g_error_free(error);
+  } else {
+    gtk_style_context_add_provider_for_screen(gdk_screen_get_default(),
+                                              GTK_STYLE_PROVIDER(provider),
+                                              GTK_STYLE_PROVIDER_PRIORITY_USER);
+  }
+  g_object_unref(provider);
 
   ventana = gtk_window_new(GTK_WINDOW_TOPLEVEL);
   gtk_window_set_title(GTK_WINDOW(ventana), titulo);
--- src/funciones/menu.c
+++ src/funciones/menu.c
@@ -2,9 +2,9 @@
 #include "../prototipos/gui.h"
 #include "../prototipos/sistema.h"
 #include <menu.h>
+#include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ncurses.h>
 
 /*NOTA:
 Cada pieza define su tipo con una char, siendo:
@@ -19,26 +19,37 @@ C: Caballo
 tipo, color, capturada Pieza torreNegra =   {1, 6, 5, 'T', 1, 0}; //
 CoordenadaX, CoordenadaY, valor, tipo, color, capturada
 */
-
 void jugar() {
-  GtkWidget *grid;
-  debugMessage("Inicializando juego...");
+    debugMessage("Inicializando juego...");
+    GtkWidget *grid;
+    GtkWidget *ventana = crearVentana("Ajedrez", 800, 800);
 
-  GtkWidget *ventana = crearVentana("Ajedrez", 800, 800);
+    grid = gtk_grid_new();
+    gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
+    gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            GtkWidget *casilla = gtk_toggle_button_new();
+            gtk_grid_attach(GTK_GRID(grid), casilla, j, i, 1, 1);
 
-  grid = gtk_grid_new();
-  gtk_grid_set_row_homogeneous(GTK_GRID(grid), TRUE);
-  gtk_grid_set_column_homogeneous(GTK_GRID(grid), TRUE);
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      GtkWidget *casilla = gtk_button_new();
-      gtk_grid_attach(GTK_GRID(grid), casilla, j, i, 1, 1);
+            // Agrega la clase "casilla-blanca" o "casilla-negra" dependiendo de la posición
+            if ((i + j) % 2 == 0) {
+                gtk_style_context_add_class(gtk_widget_get_style_context(casilla), "casilla-blanca");
+            } else {
+                gtk_style_context_add_class(gtk_widget_get_style_context(casilla), "casilla-negra");
+            }
+
+            // Agrega una imagen a la casilla
+            GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file("src/assets/amogus.jpg", NULL);
+            GdkPixbuf *scaled_pixbuf = gdk_pixbuf_scale_simple(pixbuf, 50, 50, GDK_INTERP_BILINEAR);
+            GtkWidget *imagen = gtk_image_new_from_pixbuf(scaled_pixbuf);
+            gtk_button_set_image(GTK_BUTTON(casilla), imagen);
+        }
     }
-  }
-  gtk_container_add(GTK_CONTAINER(ventana), grid);
-  g_signal_connect(ventana, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-  gtk_widget_show_all(ventana);
-  gtk_main();
+    gtk_container_add(GTK_CONTAINER(ventana), grid);
+    g_signal_connect(ventana, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+    gtk_widget_show_all(ventana);
+    gtk_main();
 }
 
 void salir() { exit(0); }
@@ -69,7 +80,7 @@ void inicializarMenu() {
     gtk_widget_set_margin_start(boton, 50);
     gtk_widget_set_margin_end(boton, 50);
     gtk_widget_set_margin_top(boton, 10);
-    gtk_widget_set_margin_bottom(boton, 10);
+    gtk_widget_set_margin_bottom(boton, 50);
     gtk_box_pack_start(GTK_BOX(box), boton, TRUE, TRUE, 0);
   }
 
@@ -80,6 +91,10 @@ void inicializarMenu() {
 }
 
 void limpiezaDePantalla() {
-  int 
-  
+  int i = detectorDeSistema();
+  if (i == 1) {
+    system("cls");
+  } else {
+    system("clear");
+  }
 }

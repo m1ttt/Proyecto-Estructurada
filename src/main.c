d3bcc5ad69a7e1ea3e0ffa034b4adfc1ff2d9a30
m1ttt <angel.moreno@iteso.mx>
2024-05-05 02:05:47 -0600

    .

--- src/main.c
+++ src/main.c
@@ -6,6 +6,28 @@
 int main() {
   inicializarMenu();
 
+/*
+  Pieza peon = {NULL, 'P', 1, 0, 6, 0};
+  //Tablero vacio, solo con el peon
+  Tablero tablero;
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      tablero.casillas[j][i] = NULL;
+    }
+  }
+  tablero.casillas[0][6] = &peon;
+  imprimirTablero(&tablero);
+
+  //Array de movimientos posibles
+  Move *movimientos = obtenerMovimientosArray(&tablero, &peon);
+  for (int i = 0; i < numMovimientos; i++) {
+    printf("x: %d, y: %d\n", movimientos[i].x, movimientos[i].y);
+  }
+  moverPieza(&tablero, &peon, 0, 1);
+  imprimirTablero(&tablero);
+  
+*/
+
   return 0;
 }
 

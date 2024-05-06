5c427323ac63e8fc5d9aa2b610bcd71813e8a37a
PapaCerdito1503 <emifragon13@gmail.com>
2024-05-06 12:43:12 -0600

    Prueba CLI

--- src/main.c
+++ src/main.c
@@ -5,21 +5,37 @@
 
 int main() {
 
-  inicializarMenu();
-  // Tablero *tablero = inicializarTableroBackend();
-  // Pieza *piezasBlancas = crearPiezasBlancas();
-  // Pieza *piezasNegras = crearPiezasNegras();
-  // colocarPiezasEnTablero(tablero, piezasBlancas, piezasNegras);
-  // imprimirTablero(tablero);
-  // printf("Las piezas blancas son: \n");
-  // for (int i = 0; i < 16; i++) {
-  //   printf("Pieza %d: %c\n", i, piezasBlancas[i].tipo);
-  // }
+  // inicializarMenu();
+  Tablero *tablero = inicializarTableroBackend();
+  Pieza *piezasBlancas = crearPiezasBlancas();
+  Pieza *piezasNegras = crearPiezasNegras();
+  colocarPiezasEnTablero(tablero, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  printf("Las piezas blancas son: \n");
+  for (int i = 0; i < 16; i++) {
+    printf("%c %d\n", piezasBlancas[i].tipo, i);
+  }
+  moverPieza(tablero, &piezasBlancas[6], 6, 4, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[13], 6, 6, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[13], 1, 1, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[13], 1, 2, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[3], 3, 4, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[14], 3, 5, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[14], 4, 4, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasBlancas[14], 4, 1, piezasBlancas, piezasNegras);
+  imprimirTablero(tablero);
+  moverPieza(tablero, &piezasNegras[0], 0, 2, piezasNegras, piezasBlancas);
+  imprimirTablero(tablero);
 
-  // Move *movimientos = obtenerMovimientosArray(tablero, &piezasBlancas[8],
-  // piezasBlancas, piezasNegras); printf("Los movimientos posibles son: \n");
-  // for (int i = 0; i < numMovimientos; i++) {
-  //   printf("Movimiento %d: %d, %d\n", i, movimientos[i].x, movimientos[i].y);
-  // }
+
+
+
   return 0;
 }
